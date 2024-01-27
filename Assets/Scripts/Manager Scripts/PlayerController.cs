03c4f019ab514f45ec263d119f849d3d351e10bc
kanuki00 <kalevi.toivanen@gmail.com>
2024-01-27 20:27:18 +0200

    slope sliding and jumping off ramps works

--- Assets/Scripts/Manager Scripts/PlayerController.cs
+++ Assets/Scripts/Manager Scripts/PlayerController.cs
@@ -35,7 +35,15 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] 
     LayerMask _slopeMask;
-    
+
+    bool _shouldMoveCameraToPlayerCamLoc = true;
+    bool _canUpdatePlayerCamRotations = true;
+
+    Vector3 _globalSlopeDirection = Vector3.right;
+
+    bool _isOnRamp = false;
+
+    bool _isOnGround = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -81,21 +89,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void UpdateCamera()
+    void UpdatePlayerCamera()
     {
-        float ffInd = Time.deltaTime / (1.0f / 60.0f) * 3.0f;
-        _cameraYAngle += _lookInput.x * _cameraSensitivity * (_isGamepad ? ffInd : 1.0f);
-        _cameraXAngle -= _lookInput.y * _cameraSensitivity * (_isGamepad ? ffInd : 1.0f);
-        _cameraXAngle = Mathf.Clamp(_cameraXAngle, -90, 90);
+        if (_canUpdatePlayerCamRotations)
+        {
+            float ffInd = Time.deltaTime / (1.0f / 60.0f) * 3.0f;
+            _cameraYAngle += _lookInput.x * _cameraSensitivity * (_isGamepad ? ffInd : 1.0f);
+            _cameraXAngle -= _lookInput.y * _cameraSensitivity * (_isGamepad ? ffInd : 1.0f);
+            _cameraXAngle = Mathf.Clamp(_cameraXAngle, -90, 90);
+        }
         
         _forwardVec = Quaternion.AngleAxis(_cameraYAngle, Vector3.up) *  new Vector3(0.0f, 0.0f, 1.0f);
         _rightVec = Vector3.Cross(_forwardVec, Vector3.up);
 
         Vector3 camPos = -_forwardVec;
         camPos = Quaternion.AngleAxis(-_cameraXAngle, _rightVec) * camPos;
-        
-        _cam.transform.position = _rb.position + camPos*_cameraBoomLength;
-        _cam.transform.rotation = Quaternion.Euler(_cameraXAngle, _cameraYAngle, 0.0f);
+
+        if (_shouldMoveCameraToPlayerCamLoc)
+        {
+            _cam.transform.position = _rb.position + camPos*_cameraBoomLength;
+            _cam.transform.rotation = Quaternion.Euler(_cameraXAngle, _cameraYAngle, 0.0f);
+        }
     }
 
     void DrawDebugPoint(Vector3 origin, Color color)
@@ -109,6 +123,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        _rb.AddForce(Vector3.down*9.81f, ForceMode.Acceleration);
+        if (_isOnGround)
+        {
+            _rb.velocity = new Vector3(0.0f, _rb.velocity.y, 0.0f);
+        }
+        
         Vector3 moveDirection = _forwardVec * _moveInput.y - _rightVec * _moveInput.x;
         
         if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit1, 1.2f))
@@ -124,21 +144,44 @@ public class PlayerController : MonoBehaviour
             Debug.DrawLine(transform.position, transform.position + slopeAxis, Color.blue);
             Debug.DrawLine(transform.position, transform.position + moveDirection, Color.green);
             Debug.Log(slopeAngle);
+
+            _isOnGround = true;
         }
         else
         {
-            Debug.Log("No hit");
+            _isOnGround = false;
         }
-
-        _rb.AddForce(Vector3.down*9.81f, ForceMode.Acceleration);
-        _rb.velocity = new Vector3(0.0f, _rb.velocity.y, 0.0f);
         
-        _rb.MovePosition(_rb.position + Time.fixedDeltaTime * _moveSpeed * moveDirection);
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit2, 1.2f, _slopeMask))
+        {
+            Debug.Log("On Slope");
+
+            Vector3 slopeAxis = Vector3.Cross(_globalSlopeDirection, hit2.normal);
+            Vector3 slopeSlideDir = Vector3.Cross(hit2.normal, slopeAxis);
+            Debug.DrawLine(transform.position, transform.position + slopeSlideDir, Color.yellow);
+            
+            _rb.MovePosition(_rb.position + Time.fixedDeltaTime * 20.0f * slopeSlideDir + Time.fixedDeltaTime * _moveSpeed * moveDirection);
+
+            if (Vector3.Dot(slopeSlideDir, Vector3.up) > 0.0f)
+            {
+                _isOnRamp = true;
+            }
+        }
+        else
+        {
+            _rb.MovePosition(_rb.position + Time.fixedDeltaTime * _moveSpeed * moveDirection);
+            if (_isOnRamp)
+            {
+                Debug.Log("Launch off ramp!");
+                _rb.AddForce(_globalSlopeDirection*10.0f, ForceMode.Impulse);
+                _isOnRamp = false;
+            }
+        }
     }
 
     private void LateUpdate()
     {
-        UpdateCamera();
+        UpdatePlayerCamera();
         Orient();
     }
 }
