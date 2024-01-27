05a197d4a32b1a9c98e01e4df6ed99fe27f8667f
Juho Selenius <juho.selenius@gmail.com>
2024-01-27 11:50:22 +0200

    Started on House script

---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    int residentsAmount;
+    public int maxResidents;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        residentsAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Rigidbody[] rbList = gameObject.GetComponentsInChildren<Rigidbody>();
+            Vector3 playerDirection = other.transform.forward;
+
+            foreach (Rigidbody rb in rbList)
+            {
+                rb.isKinematic = false;
+                Vector3 forceDirection = new Vector3(playerDirection.x, playerDirection.y + 10.0f, playerDirection.z);
+                rb.AddForce(Vector3.Normalize(forceDirection), ForceMode.Impulse);
+            }
+        }
+    }
+}
